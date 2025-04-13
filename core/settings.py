f62a3145eedc0281f2ca370cfe2fba9340ffb5a6
Peter Oyelegbin <peteroyelegbin@gmail.com>
2025-04-13 15:38:23 +0100

    added cors cofig

--- core/settings.py
+++ core/settings.py
@@ -172,12 +172,11 @@ SITE_ID = 1
 
 
 # cors policy config
-CORS_ALLOWED_ORIGINS = ["http://localhost:5173", "http://127.0.0.1:5173", "https://dwellingbloom.netlify.app"]
+# CORS_ALLOWED_ORIGINS = ["http://localhost:5173", "http://127.0.0.1:5173", "https://dwellingbloom.netlify.app"]
 
-CSRF_TRUSTED_ORIGINS = ["https://dwellingbloom.netlify.app"]
+CSRF_TRUSTED_ORIGINS = ['http://*', 'https://*', 'https://dwellingbloom.netlify.app']
 
-# CORS_ORIGIN_ALLOW_ALL = True
-# CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # static files config
